2481deb11524b024af1101b07a6141f29121bd8d
daniele.gattuso <d.gattuso09@gmail.com>
2025-01-08 17:33:55 +0100

    done 1st excercize

--- Palindromo/js/script.js
+++ Palindromo/js/script.js
@@ -3,3 +3,39 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+
+function parolaInvertita(inversa) {
+
+    return inversa.split("").reverse().join("");
+    
+}
+
+function palindromo(pU, pF) {
+
+    return pU === pF
+    
+}
+
+
+
+
+
+let parolaUtente = prompt("Inserisci una parola e vediamo se è palindroma!");
+let parolaFinale = parolaInvertita(parolaUtente);
+
+
+
+
+
+if (!palindromo(parolaUtente, parolaFinale)){
+
+    console.log("La parola non è palindroma");
+    
+
+}else {
+    console.log("La parola è palindroma");
+    
+}
+
+
